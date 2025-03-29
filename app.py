e224cce7be93823ffadbd2d10cdd6464855f924f
Hardyanice <dassouhardya1720@gmail.com>
2025-03-30 01:08:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ wastage_model = joblib.load('svm_model.pkl')
 expected_features = model.feature_names_in_
 wastage_features = joblib.load('wastage_model_features.pkl') 
 
-# Load the dataset
+# Load the dataset-1
 df = pd.read_csv('Prediction Data')
 df.drop(columns=['Unnamed: 0'], inplace=True, errors='ignore')
 
@@ -20,6 +20,9 @@ year_mapping = {2020: 2023, 2021: 2024, 2022: 2025}
 
 # Apply the mapping to the "Year" column
 df["Year"] = df["Year"].replace(year_mapping)
+
+# Load dataset-2
+df1=pd.read_csv("Waste prediction data")
 
 page = st.sidebar.selectbox("Choose Predictor", ["Food Shortage Prediction", "Food Wastage Level Prediction"])
 
@@ -45,19 +48,18 @@ if page == "Food Shortage Prediction":
             
             # Ensure columns match model expectations
             input_data = input_data.reindex(columns=expected_features, fill_value=0)
-            prediction = model.predict(input_data)
+            prediction = shortage_model.predict(input_data)
             st.write(f'Predicted Food Shortage chance: {round(prediction[0], 2) * 100}%')
-            
 
 elif page == "Food Wastage Level Prediction":
     st.header('Food Wastage Level Prediction')
     st.write('Enter a country and year to predict the food wastage level.')
     
     country = st.selectbox('Select Country', df['Country'].unique(), key='wastage_country')
-    year = st.selectbox('Select Year', sorted(df['Year'].unique()), key='wastage_year')
+    year = st.selectbox('Select Year', sorted(df1['Year'].unique()), key='wastage_year')
     
     if st.button('Predict', key='wastage_predict'):
-        input_data = df[(df['Country'] == country) & (df['Year'] == year)]
+        input_data = df1[(df1['Country'] == country) & (df1['Year'] == year)]
         
         if input_data.empty:
             st.write('No data available for the selected country and year.')
@@ -68,9 +70,4 @@ elif page == "Food Wastage Level Prediction":
             st.write(f'Predicted Food Wastage Level: {prediction[0]}')
 
             
-            # Make prediction
-            prediction = model.predict(input_data)
-            
-            # Display prediction
-            st.write(f'Predicted Food Shortage chance: {round(prediction[0], 2) * 100}%')
-
+            x
